22c02d216627e5a8f79e5abafa3e610f459b9471
Sanu Khan-Business Partner <sanu.khan-external@alfuttaim.com>
2023-06-09 10:55:50 +0400

    Final fix & update

--- js/product-3d-model.js
+++ js/product-3d-model.js
@@ -1,49 +1,61 @@
 function receiveDataFromPopup(data) {
-    console.log("Data received from popup: ", data);
-    // process the data as needed...
+  console.log("Data received from popup: ", data);
+  // process the data as needed...
 }
 
 // Listen for messages from the popup
 
-window.addEventListener('message', function(event) {
+window.addEventListener(
+  "message",
+  function (event) {
     // Check the origin of the data to ensure it's what you expect
-    if (event.origin !== 'http://127.0.0.1:5173', !event.data ) {
+    if (
+      (event.origin !== "https://phpstack-947027-3534862.cloudwaysapps.com" ||
+      !event.data)
+    ) {
       return;
     }
-    
-    console.log('Received data: ', JSON.parse(event.data));
-    this.localStorage.setItem('customizeData', event.data);
-  }, false);
+    console.log("Received data: ", JSON.parse(event.data));
+    this.localStorage.setItem("customizeData", event.data);
+  },
+  false
+);
 
+function jsonToReadableText(json) {
+  let readableText = "Meterial Desing - \n";
+  for (const [key, value] of Object.entries(json)) {
+    const material = key.split("-")[1];
+    readableText += `${material} : ${value}\n`;
+  }
+  return readableText;
+}
 
+jQuery(document).ready(function ($) {
+  $("form.cart").on("submit", function (e) {
+    e.preventDefault();
+    let materialsJson = JSON.parse(localStorage.getItem("customizeData"));
+    let product_id = $(this).find('button[name="add-to-cart"]').val();
+    let data = {
+      action: "add_custom_data",
+      product_id: product_id,
+      custom_option: jsonToReadableText(materialsJson),
+      quantity: $(this).find('input[name="quantity"]').val(),
+    };
 
-  jQuery(document).ready(function($) {
-    $('form.cart').on('submit', function(e) {
-        e.preventDefault();
+    $.post(wc_add_to_cart_params.ajax_url, data, function (response) {
+      if (!response) {
+        console.log("Invalid response");
+        return;
+      }
 
-        
-        let materialsJson = JSON.stringify(localStorage.getItem('customizeData'));
-        let product_id = $(this).find('button[name="add-to-cart"]').val();
-        let data = {
-            'action': 'add_custom_data',
-            'product_id': product_id,
-            'custom_option': materialsJson,
-            'quantity': $(this).find('input[name="quantity"]').val()
-        };
+      if (response.error && response.product_url) {
+        window.location = response.product_url;
+        return;
+      }
 
-        $.post(wc_add_to_cart_params.ajax_url, data, function(response) {
-            if (!response) {
-                console.log('Invalid response');
-                return;
-            }
-
-            if (response.error && response.product_url) {
-                window.location = response.product_url;
-                return;
-            }
-
-            // Redirect to the cart page
-            window.location = wc_add_to_cart_params.cart_url;
-        });
+      localStorage.removeItem("customizeData");
+      // Redirect to the cart page
+      window.location = wc_add_to_cart_params.cart_url;
     });
+  });
 });
--- product-3d-model.php
+++ product-3d-model.php
@@ -43,16 +43,13 @@ function display_3d_model_on_product_page() {
     $model_url = get_post_meta( $product_id, '_3d_model_url', true );
 
     if ( ! empty( $model_url ) ) {
-        // Modify this URL to point to your external page for viewing 3D model.
-        // The external page should handle loading three.js, GLTFLoader.js, and the 3D model.
-        $external_viewer_url = "http://127.0.0.1:5173/?modelUrl=";
+        $external_viewer_url = "https://phpstack-947027-3534862.cloudwaysapps.com/?modelUrl=";
         $full_url = $external_viewer_url . urlencode($model_url);
         echo '<div id="product-3d-button">';
         echo '<button class="button-3d" onclick="window.open(\'' . esc_url( $full_url ) . '\', \'_blank\', \'toolbar=no,scrollbars=yes,resizable=yes,width=900,height=400\')"><i class="icon-3d"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-badge-3d-fill" viewBox="0 0 16 16"> <path d="M10.157 5.968h-.844v4.06h.844c1.116 0 1.621-.667 1.621-2.02 0-1.354-.51-2.04-1.621-2.04z"/> <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm5.184 4.368c.646 0 1.055.378 1.06.9.008.537-.427.919-1.086.919-.598-.004-1.037-.325-1.068-.756H3c.03.914.791 1.688 2.153 1.688 1.24 0 2.285-.66 2.272-1.798-.013-.953-.747-1.38-1.292-1.432v-.062c.44-.07 1.125-.527 1.108-1.375-.013-.906-.8-1.57-2.053-1.565-1.31.005-2.043.734-2.074 1.67h1.103c.022-.391.383-.751.936-.751.532 0 .928.33.928.813.004.479-.383.835-.928.835h-.632v.914h.663zM8.126 11h2.189C12.125 11 13 9.893 13 7.985c0-1.894-.861-2.984-2.685-2.984H8.126V11z"/> </svg></i> </button>';
         echo '</div>';
     }
 }
-// add_action( 'woocommerce_before_single_product_summary', 'display_3d_model_on_product_page' );
 add_action( 'woocommerce_single_product_summary', 'display_3d_model_on_product_page' );
 
 // Enqueue styles
@@ -90,7 +87,7 @@ add_action( 'wp_ajax_nopriv_add_custom_data', 'add_custom_data' );
 // Store custom field in order item meta
 function add_custom_option_order_item_meta( $item, $cart_item_key, $values, $order ) {
     if ( array_key_exists( 'custom_option', $values ) ) {
-        $item->add_meta_data( __( 'Custom Option', 'woocommerce' ), $values['custom_option'] );
+        $item->add_meta_data( __( 'Customer configration Preference: ', 'woocommerce' ), $values['custom_option'] );
     }
 }
 add_action( 'woocommerce_checkout_create_order_line_item', 'add_custom_option_order_item_meta', 10, 4 );
